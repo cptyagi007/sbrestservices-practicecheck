6a3509024746a3a3731b8b16659d2ebb3cc95de0
849728 <63786225+cptyagi007@users.noreply.github.com>
2020-06-12 19:19:37 +0530

    Add files via upload

---
+++ sbrestservicespractice-check/truyum/src/main/java/com/cognizant/truyum/TruyumApplication.java
@@ -0,0 +1,15 @@
+package com.cognizant.truyum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class TruyumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruyumApplication.class, args);
+	}
+
+}
